e22025b012c0381ca281362625f762633d3be807
Yobany Chavez <84747234+yoba95@users.noreply.github.com>
2022-10-19 14:26:46 -0500

    serverCorriendoPrueba

--- app/server.js
+++ app/server.js
@@ -21,7 +21,7 @@ app.use(require('./routers/Localidad_routes'));
 app.listen(PORT, function(){
      try {
      console.log('servidor corriendo en el puerto: ' +PORT);    
-     conect.sequelize.sync({ force:true}).then(() => {
+     conect.sequelize.sync({ alter:true}).then(() => {
      console.log("Se ha establecido la conexión");
     })
     } catch (error) {
