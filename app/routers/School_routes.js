484675e1c9bb619f071c8edce00633f2d4148760
yoba95 <thecromo95@outlook.es>
2022-11-03 13:06:01 -0600

    update server

--- app/routers/School_routes.js
+++ app/routers/School_routes.js
@@ -8,7 +8,7 @@ const { checkExistedSchool } = require('../middleware/validateUser');
 //controlle
 const SchoolController = require('../controllers/School_controller');
 //prueba de fina¿dall user con roles y roles con users
-router.get('/api/v1/schools',auth.validateToken,SchoolController.allSchool);
+router.get('/api/v1/schools',SchoolController.allSchool);
 router.post('/api/v1/school',auth.validateToken,validateCreateSchool, checkExistedSchool, SchoolController.createSchool);
 router.get('/api/v1/school/:id', SchoolController.allSchoolId);
 router.put('/api/v1/school/:id', SchoolController.updateSchool );
