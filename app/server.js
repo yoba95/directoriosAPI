297fcfddfe33f2088dc705863a90a39e997fe014
yoba95 <thecromo95@outlook.es>
2022-11-23 20:55:16 -0600

    server render

--- app/server.js
+++ app/server.js
@@ -9,7 +9,16 @@ const PORT = parseInt(process.env.PORT) || 7000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+/*var whitelist = ['http://localhost:5000', 'http://192.168.1.194:5000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}*/
 app.use(require('./routers/User_routes'));
 app.use(require('./routers/Role_routes'));
 app.use(require('./routers/Employee_routes'));
