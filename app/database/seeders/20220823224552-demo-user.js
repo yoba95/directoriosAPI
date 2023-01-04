b0f03fc2a42f0465f463579a8fdab010e24c4d67
yoba95 <thecromo95@outlook.es>
2023-01-03 21:10:46 -0600

    ssssss

--- app/database/seeders/20220823224552-demo-user.js
+++ app/database/seeders/20220823224552-demo-user.js
@@ -5,16 +5,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('users', [{
      //username: 'admin',
+      full_name: 'MARÍA DE JESÚSMELGAR VÁZQUEZ',
       email: 'yobany@gmail.com',
       password: bcrypt.hashSync("12345678", +authConfig.rounds),
+      number_phone: 'S/N',
+      oficina: 'OR ITSMO',
       roleId: 1,
       
     },
     {
      // username: 'will smith',
+      full_name: 'MARÍA DE JESÚSMELGAR VÁZQUEZ',
       email: 'kevin@gmail.com',
       password: bcrypt.hashSync("12345678", +authConfig.rounds),
-      roleId: 2,
+      number_phone: 'S/N',
+oficina: 'OR ITSMO',
+      roleId: 1,
     },
     ]
     );
